bb13e88806375e1ca5b04fb49947add4b5710535
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-03-13 13:00:36 +0700

    installation and configuration next js, other libs

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <header>
+        <a href="">PIZZA</a>
+        <nav>
+          <a href=""></a>
+        </nav>
+      </header>
+    </>
+  );
+}
